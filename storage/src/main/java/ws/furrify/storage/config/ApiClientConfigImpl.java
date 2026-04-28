e37694039eca315949467efd53060caf28fe8afe
Snowy <skyterix1991@protonmail.com>
2026-04-28 19:36:05 +0200

    Moved enable command

--- storage/src/main/java/ws/furrify/storage/config/ApiClientConfigImpl.java
+++ storage/src/main/java/ws/furrify/storage/config/ApiClientConfigImpl.java
@@ -1,8 +1,13 @@
 package ws.furrify.storage.config;
 
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 import ws.furrify.core.config.ApiConfig;
 
 @Configuration
+@EnableFeignClients(basePackages = {
+        "ws.furrify.openapi.gen",
+        "org.openapitools.configuration"
+})
 class ApiClientConfigImpl extends ApiConfig {
 }
