e4d12c4aef350a9d9a8bffdd43e2014524ef914d
Snowy <skyterix1991@protonmail.com>
2026-08-20 15:23:44 +0200

    Auto enum ref

--- core/src/main/java/ws/furrify/core/config/WebConfig.java
+++ core/src/main/java/ws/furrify/core/config/WebConfig.java
@@ -1,5 +1,8 @@
 package ws.furrify.core.config;
 
+import com.fasterxml.jackson.databind.JavaType;
+import io.swagger.v3.core.converter.ModelConverter;
+import io.swagger.v3.oas.models.media.Schema;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
@@ -8,6 +11,36 @@ import static org.springframework.data.web.config.EnableSpringDataWebSupport.Pag
 
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public abstract class WebConfig {
+
+    /**
+     * Make sure docs for enums are generated as separate enum classes not inline enums.
+     */
+    @Bean
+    public ModelConverter enumModelConverter() {
+        return (type, context, chain) -> {
+            if (chain.hasNext()) {
+                Schema<?> resolvedSchema = chain.next().resolve(type, context, chain);
+                if (resolvedSchema != null) {
+                    Class<?> clazz = null;
+                    if (type.getType() instanceof Class<?>) {
+                        clazz = (Class<?>) type.getType();
+                    } else if (type.getType() instanceof JavaType) {
+                        clazz = ((JavaType) type.getType()).getRawClass();
+                    }
+
+                    if (clazz != null && clazz.isEnum()) {
+                        resolvedSchema.setName(clazz.getSimpleName());
+                        context.defineModel(clazz.getSimpleName(), resolvedSchema);
+                        Schema<?> refSchema = new Schema<>();
+                        refSchema.$ref("#/components/schemas/" + clazz.getSimpleName());
+                        return refSchema;
+                    }
+                }
+                return resolvedSchema;
+            }
+            return null;
+        };
+    }
 
     @Bean
     public ShallowEtagHeaderFilter shallowEtagHeaderFilter() {
