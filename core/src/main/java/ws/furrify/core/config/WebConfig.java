46456821d0157a21e1ee36dbfdb6d35f36af37e0
Snowy <skyterix1991@protonmail.com>
2026-08-20 14:48:51 +0200

    Remove openapi id customizer

--- core/src/main/java/ws/furrify/core/config/WebConfig.java
+++ core/src/main/java/ws/furrify/core/config/WebConfig.java
@@ -1,6 +1,5 @@
 package ws.furrify.core.config;
 
-import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
@@ -9,23 +8,6 @@ import static org.springframework.data.web.config.EnableSpringDataWebSupport.Pag
 
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public abstract class WebConfig {
-
-    @Bean
-    public OpenApiCustomizer operationIdCustomizer() {
-        return openApi -> openApi.getPaths().values().forEach(pathItem ->
-                pathItem.readOperations().forEach(operation -> {
-                    String tagName = (operation.getTags() != null && !operation.getTags().isEmpty())
-                            ? operation.getTags().getFirst()
-                            : "Default";
-
-                    String originalId = operation.getOperationId();
-
-                    String cleanTag = tagName.replaceAll("\\s+", "");
-
-                    operation.setOperationId(cleanTag + "_" + originalId);
-                })
-        );
-    }
 
     @Bean
     public ShallowEtagHeaderFilter shallowEtagHeaderFilter() {
