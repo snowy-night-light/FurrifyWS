905753b807b02427662d2a24e3f847930996fa0f
Snowy <skyterix1991@protonmail.com>
2026-08-26 21:44:40 +0200

    Tag alias regexp

--- storage/src/main/java/ws/furrify/storage/dto/tag/alias/request/PatchTagAliasRequest.java
+++ storage/src/main/java/ws/furrify/storage/dto/tag/alias/request/PatchTagAliasRequest.java
@@ -10,12 +10,10 @@ import ws.furrify.core.entity.request.EntityIdRequest;
 import ws.furrify.storage.domain.tag.alias.TagAlias;
 import ws.furrify.storage.dto.tag.alias.TagAliasDTO;
 
-import java.util.List;
-
 @Data
 public class PatchTagAliasRequest implements BasePatchEntityRequest<TagAlias, TagAliasDTO> {
 
     private JsonNullable<@NotNull EntityIdRequest> targetTag = JsonNullable.undefined();
 
-    private JsonNullable<@NotBlank @Pattern(regexp = "^[a-z0-9 ]+$") String> alias = JsonNullable.undefined();
+    private JsonNullable<@NotBlank @Pattern(regexp = "^[a-z0-9]+(?: [a-z0-9]+)*$") String> alias = JsonNullable.undefined();
 }
