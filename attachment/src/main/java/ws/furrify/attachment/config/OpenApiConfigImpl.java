a2fb8654ff86c89c395efc8320b731afd1969ff8
Snowy <skyterix1991@protonmail.com>
2026-08-26 17:39:59 +0200

    OpenApi fix

---
+++ attachment/src/main/java/ws/furrify/attachment/config/OpenApiConfigImpl.java
@@ -0,0 +1,8 @@
+package ws.furrify.attachment.config;
+
+import org.springframework.context.annotation.Configuration;
+import ws.furrify.core.config.OpenApiConfig;
+
+@Configuration
+class OpenApiConfigImpl extends OpenApiConfig {
+}
